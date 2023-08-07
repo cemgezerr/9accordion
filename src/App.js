a0805d66694c59702a1bd9fe98863e11d560a4e2
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-08-07 20:41:07 +0300

    accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [show,setShow] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Accordion</h1>
+
+      <h3 onClick={()=> setShow(!show)}>Tıkla Açılsın</h3>
+
+      {show && <p>Selamlar</p> }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
